2f290465749df648cdc7885bac87052b573c97fb
Franco Canevali <francocanevali@gmail.com>
2020-11-09 10:58:10 -0300

    test

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
                 <small>Cerro Fitz Roy, El Chalten, Argentina. Feb 2020.</small>
             </div>
             <div class="col text-left mt-md-5">
-            Franco. Software Developer._
+            Franco. Software Developer.
             <br>
             <br>
             em: email[at]francocanevali.com
