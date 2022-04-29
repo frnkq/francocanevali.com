f54705cb2f2d6a3b31197e56bd74b297e89d6cd3
Franco Canevali <francocanevali@gmail.com>
2022-04-29 19:02:21 -0300

    repo testing

--- pages/api/blog/posts/posts.repository.js
+++ pages/api/blog/posts/posts.repository.js
@@ -1,6 +1,14 @@
 export default class PostsRepository {
   constructor() {}
+
   getPosts() {
-    return [{}, {}, {}];
+      //now instead of returning from here
+      //return from a database
+      //db factory?
+    const posts = [
+      { id: 1, title: "ASD", content: "ASD", tags: ["tag"] },
+      { id: 3, title: "ASD", content: "LOL", tags: ["asd"] },
+    ];
+    return posts;
   }
 }
